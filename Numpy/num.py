d77550037f88b9d98b620280e95ddafa6b6d67b0
Vemula Dileep <vemuladileep082002@gmail.com>
2024-10-17 12:14:26 +0530

    VEMULA add : create 3x3 matrix with values from 2 to 10

--- Numpy/num.py
+++ Numpy/num.py
@@ -14,12 +14,22 @@ def numeric_list_to_array(lst):
     """
     return np.array(lst)
 
+def create_matrix():
+    """
+    Description:
+        This function creates a 3x3 matrix with values ranging from 2 to 10.
+    Return:
+        A 3x3 matrix.
+    """
+    return np.arange(2, 11).reshape(3, 3)
 
 def main():
     # 1. Convert list to one-dimensional numpy array
     original_list = [12.23, 13.32, 100, 36.32]
     log.info(f"Original List: {original_list}")
     log.info(f"One-dimensional numpy array: {numeric_list_to_array(original_list)}")
+    # 2. Create a 3x3 matrix with values from 2 to 10
+    log.info(f"3x3 Matrix:\n{create_matrix()}")
 
 # Entry point
 if __name__ == "__main__":
